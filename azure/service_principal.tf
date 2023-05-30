10c00e93518bfe3117d461fe8a4a62b3b07db2b2
wangxiaoyou1993 <xiaoyou@mage.ai>
2023-05-29 22:28:40 -0700

    [xy] Grant more permissions.

--- azure/service_principal.tf
+++ azure/service_principal.tf
@@ -22,3 +22,28 @@ resource "azurerm_role_assignment" "storage" {
   role_definition_name  = "Storage Blob Data Reader"
   principal_id          = azuread_service_principal.app.id
 }
+
+# Custom role for creating container instance executors
+resource "azurerm_role_definition" "custom_role" {
+  name               = "${var.app_name}-custom-role"
+  scope              = data.azurerm_subscription.current.id
+
+  permissions {
+    actions     = [
+      "Microsoft.ContainerInstance/containerGroups/read",
+      "Microsoft.ContainerInstance/containerGroups/write",
+      "Microsoft.ContainerInstance/containerGroups/delete",
+    ]
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    data.azurerm_subscription.current.id,
+  ]
+}
+
+resource "azurerm_role_assignment" "custom_role" {
+  scope                 = data.azurerm_subscription.current.id
+  role_definition_id    = azurerm_role_definition.custom_role.role_definition_resource_id
+  principal_id          = azuread_service_principal.app.id
+}

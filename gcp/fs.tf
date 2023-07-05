66731e126a996f9ebc9e78d1db400228127677f5
wangxiaoyou1993 <80284865+wangxiaoyou1993@users.noreply.github.com>
2023-07-05 04:48:35 -0700

    [xy] Add option to run NFS on GCP compute engine. (#34)

--- gcp/fs.tf
+++ gcp/fs.tf
@@ -22,3 +22,26 @@ resource "google_vpc_access_connector" "connector" {
   region        = var.region
   network       = "default"
 }
+
+# ---------------------------------------------------
+# Uncomment the resource below to use a cheaper NFS provisioned on GCP Compute Engine.
+# Then update the value of "FILESTORE_IP_ADDRESS" to module.nfs.internal_ip
+# and update the value of "FILE_SHARE_NAME" to "share/mage"
+# ---------------------------------------------------
+#
+# module "nfs" {
+#   source  = "DeimosCloud/nfs/google"
+#   version = "1.0.1"
+#   # insert the 5 required variables here
+#   name_prefix = "${var.app_name}-nfs"
+#   # labels      = local.common_labels
+#   # subnetwork  = module.vpc.public_subnetwork
+#   attach_public_ip = true
+#   project     = var.project_id
+#   network     = "default"
+#   machine_type = "e2-medium"
+#   export_paths = [
+#     "/share/mage",
+#   ]
+#   capacity_gb = "100"
+# }

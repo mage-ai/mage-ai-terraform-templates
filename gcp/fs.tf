9715ad713042a181b04537d30c89d834e69ab196
wangxiaoyou1993 <80284865+wangxiaoyou1993@users.noreply.github.com>
2023-07-07 03:45:03 -0700

    [xy] Update NFS resource and add comments. (#35)

--- gcp/fs.tf
+++ gcp/fs.tf
@@ -24,11 +24,14 @@ resource "google_vpc_access_connector" "connector" {
 }
 
 # ---------------------------------------------------
-# Uncomment the resource below to use a cheaper NFS provisioned on GCP Compute Engine.
-# Then update the value of "FILESTORE_IP_ADDRESS" to module.nfs.internal_ip
-# and update the value of "FILE_SHARE_NAME" to "share/mage"
+# Use a cheaper NFS provisioned on GCP Compute Engine.
+# 1. Uncomment the module "nfs" below
+# 2. Update the environment variable in google_cloud_run_service.run_service.
+#    Set "FILESTORE_IP_ADDRESS" to module.nfs.internal_ip and
+#    "FILE_SHARE_NAME" to "share/mage".
+# 3. Comment out the google_filestore_instance.instance resource.
 # ---------------------------------------------------
-#
+
 # module "nfs" {
 #   source  = "DeimosCloud/nfs/google"
 #   version = "1.0.1"
@@ -37,11 +40,13 @@ resource "google_vpc_access_connector" "connector" {
 #   # labels      = local.common_labels
 #   # subnetwork  = module.vpc.public_subnetwork
 #   attach_public_ip = true
-#   project     = var.project_id
-#   network     = "default"
-#   machine_type = "e2-small"
-#   export_paths = [
+#   project       = var.project_id
+#   network       = "default"
+#   machine_type  = "e2-small"
+#   source_image_project  = "debian-cloud"
+#   image_family  = "debian-11-bullseye-v20230629"
+#   export_paths  = [
 #     "/share/mage",
 #   ]
-#   capacity_gb = "100"
+#   capacity_gb = "50"
 # }
